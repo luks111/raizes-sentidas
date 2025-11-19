25c3de96a38e6389b9592d6de7f454aa24ef86fc
luks111 <llmuniz@aluno.crb.g12.br>
2025-11-18 23:06:25 -0300

    Adicionando Sobrenome e enter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [msg, setMsg] = useState("Carregando...");
+  const [texto, setTexto] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:5000/ping")
@@ -10,9 +11,45 @@ function App() {
       .catch(() => setMsg("Erro ao conectar com o servidor"));
   }, []);
 
+  function enviarParaServer() {
+    fetch("http://localhost:5000/enviar", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ mensagem: texto })
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log("Resposta do servidor:", data);
+
+       // alert("Servidor recebeu: " + data.recebido);
+       
+      })
+      .catch(err => console.error("Erro:", err));
+  }
+
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
+    <div className="flex items-center justify-center h-screen bg-gray-100 flex-col gap-4">
+
       <h1 className="text-2xl font-bold">{msg}</h1>
+
+      <input
+        type="text"
+        value={texto}
+        onChange={e => setTexto(e.target.value)}
+        placeholder="Digite algo..."
+        className="border border-gray-400 p-2 rounded w-64"
+      />
+
+      <button
+        onClick={enviarParaServer}
+        className="bg-blue-600 text-white px-4 py-2 rounded"
+      >
+      Enviar
+      </button>
+
+
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -4,12 +4,24 @@ import cors from "cors";
 const app = express();
 const PORT = 5000;
 
-// Permite que o React acesse o servidor
+// OBRIGATÓRIO PARA LER JSON
+app.use(express.json());
+
 app.use(cors());
 
 // Rota de teste
 app.get("/ping", (req, res) => {
   res.send("Servidor do Projeto C4 está rodando!");
+});
+
+// Rota que recebe os dados do front
+app.post("/enviar", (req, res) => {
+  console.log("BODY RECEBIDO:", req.body);
+
+  res.json({
+    sucesso: true,
+    recebido: req.body.mensagem || "nada recebido"
+  });
 });
 
 app.listen(PORT, () => {
